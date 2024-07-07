e9fe8440ff5942d09f76a6cf982d371e36e1983e
Sim-GyuBin <skb0606@gmail.com>
2024-07-07 14:39:53 +0900

    init: 프로젝트 구조 생성

---
+++ src/main/java/com/gdsc/illuwabang/IlluwabangApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.illuwabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlluwabangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlluwabangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdsc/illuwabang/IlluwabangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.illuwabang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlluwabangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
