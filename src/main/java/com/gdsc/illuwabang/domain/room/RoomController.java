4e8cfb81074e3dad93a9e111252cfaea223e98a9
Sim-Gyubin <skb0606@gmail.com>
2024-07-21 23:34:05 +0900

    feat: 전체 방 조회 및 방 등록 controller

---
+++ src/main/java/com/gdsc/illuwabang/domain/room/RoomController.java
@@ -0,0 +1,45 @@
+package com.gdsc.illuwabang.domain.room;
+
+import com.gdsc.illuwabang.domain.room.dto.RoomRegisterDto;
+import com.gdsc.illuwabang.domain.user.User;
+import com.gdsc.illuwabang.domain.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class RoomController {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    RoomService roomService;
+
+
+    @ResponseBody
+    @GetMapping("/allRooms")
+    public ResponseEntity<?> getAllRooms(Authentication authentication) {
+        return ResponseEntity.ok().body(roomService.getAllRooms());
+    }
+
+    @PostMapping("/rooms/register")
+    public ResponseEntity<?> registerRoom(Authentication authentication, @RequestBody RoomRegisterDto roomInfo) {
+        Long userId = 0L;
+        Optional<User> exist_user = userService.findBySub(authentication.getName());
+        if (exist_user.isPresent()) {
+            User user = exist_user.get();
+            userId = user.getId();
+        } else {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+        return ResponseEntity.ok().body(roomService.registerRoom(userId, roomInfo));
+    }
+}
