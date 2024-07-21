e51ae38dc17d5ee402ef9266dd30f00309b43ecf
Sim-Gyubin <skb0606@gmail.com>
2024-07-21 23:33:28 +0900

    feat: allRooms 사용 dto

---
+++ src/main/java/com/gdsc/illuwabang/domain/room/dto/RoomResponseDto.java
@@ -0,0 +1,22 @@
+package com.gdsc.illuwabang.domain.room.dto;
+
+import com.gdsc.illuwabang.domain.room.State;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomResponseDto {
+    private Long roomId;
+    private String thumbnail;
+    private String type;
+    private Integer deposit;
+    private Integer rent;
+    private String roadAddress;
+    private String floor;
+    private String startDate;
+    private String endDate;
+    private State state;
+    private Double latitude;
+    private Double longitude;
+}
