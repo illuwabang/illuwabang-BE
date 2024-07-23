227edbe0bb3e46c00d0b0c19a88f4ab46ef73d56
Sim-Gyubin <skb0606@gmail.com>
2024-07-23 22:26:31 +0900

    feat: 매물 type enum

---
+++ src/main/java/com/gdsc/illuwabang/domain/room/Type.java
@@ -0,0 +1,15 @@
+package com.gdsc.illuwabang.domain.room;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Type {
+    ONE_ROOM("원룸"),
+    SHORT_TERM_ONE_ROOM("단기원룸"),
+    SHARE_HOUSE("쉐어하우스"),
+    ROOMMATE("자취메이트");
+
+    private final String type;
+}
