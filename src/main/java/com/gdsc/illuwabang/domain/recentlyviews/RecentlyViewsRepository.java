3285f8ceb3801c98098b06e0cd85ddef6ecbd5a3
Sim-Gyubin <skb0606@gmail.com>
2024-08-08 18:24:40 +0900

    feat: 홈 화면 데이터 가져오는 jpql 리포지토리 코드

--- src/main/java/com/gdsc/illuwabang/domain/recentlyviews/RecentlyViewsRepository.java
+++ src/main/java/com/gdsc/illuwabang/domain/recentlyviews/RecentlyViewsRepository.java
@@ -1,5 +1,7 @@
 package com.gdsc.illuwabang.domain.recentlyviews;
 
+import com.gdsc.illuwabang.domain.home.HomeDao;
+import com.gdsc.illuwabang.domain.home.HomeSelectRoomDto;
 import com.gdsc.illuwabang.domain.room.Room;
 import com.gdsc.illuwabang.domain.user.User;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +14,9 @@ import java.util.List;
 
 @Repository
 public interface RecentlyViewsRepository extends JpaRepository<RecentlyViews, Long> {
-    @Query("SELECT rv FROM RecentlyViews rv WHERE rv.user = :user ORDER BY rv.viewDate DESC")
-    List<RecentlyViews> findTop10ByUser(@Param("user") User user, Pageable pageable);
+    @Query("SELECT new com.gdsc.illuwabang.domain.home.HomeDao(rv.room.id, rv.room.imageUrl) " +
+            "FROM RecentlyViews rv " +
+            "WHERE rv.user = :user " +
+            "ORDER BY rv.viewDate DESC")
+    List<HomeDao> findTop10ByUser(@Param("user") User user, Pageable pageable);
 }
