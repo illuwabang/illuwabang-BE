46b2ef0f846e79c2341729d325230054a0ea0ab8
Sim-Gyubin <skb0606@gmail.com>
2024-08-08 18:21:46 +0900

    feat: ImageUrl 빌더 패턴 추가

--- src/main/java/com/gdsc/illuwabang/domain/room/ImageUrl.java
+++ src/main/java/com/gdsc/illuwabang/domain/room/ImageUrl.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
+@AllArgsConstructor @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ToString
 public class ImageUrl {
